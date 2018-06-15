e5d5160f6f071d4b69fbe75886a0878fa51366a8
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-06-15 14:13:06 -0300

    Body-parser adicionado e formulário feito

--- portal-de-noticias/app/routes/formulario_adicao_noticia.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-    app.get("/formulario_adicao_noticia", function(req, res){
-        res.render("admin/form_add_noticia");
-    });
-};
--- portal-de-noticias/config/server.js
+++ portal-de-noticias/config/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var consign = require("consign");
+var bodyParser = require("body-parser");
 var app = express();
 
 // Indica o ejs como view engine do projeto
@@ -7,6 +8,9 @@ app.set("view engine", "ejs");
 
 // Configurar path das views
 app.set("views", "./app/views");
+
+// Middleware Body-parser
+app.use(bodyParser.urlencoded({extended : true}));
 
 // Configurar path das rotas
 consign()
