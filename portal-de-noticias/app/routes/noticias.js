dcb365d20233d152275945e41ca5978053ca39bb
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-06-23 01:54:11 -0300

    Rota noticias.js refatorada

--- portal-de-noticias/app/routes/noticias.js
+++ portal-de-noticias/app/routes/noticias.js
@@ -10,4 +10,16 @@ module.exports = function(app){
         });
 
     });
+
+    app.get("/noticia", function(req, res){
+
+        var connection = app.config.dbConnection();
+        var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+        noticiasModel.getNoticia(function(error, result){
+            res.render("noticias/noticia", {noticia : result});
+        });
+
+    });
+    
 }
