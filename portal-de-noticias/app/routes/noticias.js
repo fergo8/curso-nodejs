8496afe9446e6d1041df42f65f8389be82db767e
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-06-23 02:06:46 -0300

    Controller noticias.js criado

--- portal-de-noticias/app/routes/noticias.js
+++ portal-de-noticias/app/routes/noticias.js
@@ -1,25 +1,11 @@
 module.exports = function(app){
 
     app.get("/noticias", function(req, res){
-
-        var connection = app.config.dbConnection();
-        var noticiasModel = new app.app.models.NoticiasDAO(connection);
-        
-        noticiasModel.getNoticias(function(error, result){
-            res.render("noticias/noticias", {noticias : result});
-        });
-
+        app.app.controllers.noticias.noticias(app, req, res);
     });
 
     app.get("/noticia", function(req, res){
-
-        var connection = app.config.dbConnection();
-        var noticiasModel = new app.app.models.NoticiasDAO(connection);
-
-        noticiasModel.getNoticia(function(error, result){
-            res.render("noticias/noticia", {noticia : result});
-        });
-
+        app.app.controllers.noticias.noticia(app, req, res);
     });
     
 }
