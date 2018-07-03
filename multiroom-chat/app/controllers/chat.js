bc8a6c3ca865febbb632ebffe3e7f9d3af3bb1bf
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-07-03 02:49:15 -0300

    Validadores de formulário adicionados

--- multiroom-chat/app/controllers/chat.js
+++ multiroom-chat/app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.iniciaChat = function(app, req, res){
+
+    var dadosForm = req.body;
+    
+    req.assert("apelido", "Apelido é obrigatório").notEmpty();
+    req.assert("apelido", "Apelido deve ter entre 3 e 15 caracteres").len(3,15);
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render("index", {valid : erros});
+        return;
+    }
+
     res.render("chat");
 }
