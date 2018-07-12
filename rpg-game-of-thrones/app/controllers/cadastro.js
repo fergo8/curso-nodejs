70f52e16862e97cfff1a6e21e9515166e6351cb5
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-07-11 22:07:04 -0300

    Validação de dados de formulário criada

--- rpg-game-of-thrones/app/controllers/cadastro.js
+++ rpg-game-of-thrones/app/controllers/cadastro.js
@@ -1,3 +1,22 @@
 module.exports.cadastro = function(app, req, res){
-    res.render("cadastro");
+    res.render("cadastro", { valid : {}, dadosForm : {} });
+}
+
+module.exports.cadastrar = function (app, req, res) {
+    
+    var dadosForm = req.body;
+
+    req.assert("nome", "Nome não pode ficar vazio").notEmpty();
+    req.assert("usuario", "Usuário não pode ficar vazio").notEmpty();
+    req.assert("senha", "Senha não pode ficar vazio").notEmpty();
+    req.assert("casa", "Casa não pode ficar vazio").notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render("cadastro", { valid : erros, dadosForm : dadosForm });
+        return;
+    }
+
+    res.send("cadastrar");
 }
