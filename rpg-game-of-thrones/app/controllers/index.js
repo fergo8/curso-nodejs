ff8b81c607962d4c3935dde2fc8a4132dbaf26e7
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-07-15 00:32:57 -0300

    Validação de dados do form na página index criada

--- rpg-game-of-thrones/app/controllers/index.js
+++ rpg-game-of-thrones/app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.index = function(app, req, res){
     res.render("index");
 }
+
+module.exports.autenticar = function(app, req, res){
+
+    var dadosForm = req.body;
+
+    req.assert("usuario", "Usuário não pode ficar vazio").notEmpty();
+    req.assert("senha", "Senha não pode ficar vazia").notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render("index", { valid : erros } );
+        return;
+    }
+
+    res.send("wappa woo!");
+}
