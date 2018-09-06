81d1e628ba9b59cafad65bb34ad7774b22c50a05
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-09-06 03:09:28 -0300

    Usando método format() para configurar saída HTML ou JSON

--- apps-cliente-servidor/servidor/app/routes/index.js
+++ apps-cliente-servidor/servidor/app/routes/index.js
@@ -1,5 +1,19 @@
 module.exports = function(application){
 	application.get('/', function(req, res){
-		res.send('Bem-vindo à app NodeJS!');
+
+		res.format({		// format() = indica em qual formato será o response
+			html : function(){
+				res.send('Bem-vindo à app NodeJS!');
+			},
+
+			json : function(){
+				var retorno = {
+					body: "Bem-vindo à app NodeJS!"
+				}
+				res.json(retorno);		// json() = retorna um json
+			}
+		});
+
+		
 	});
 }
