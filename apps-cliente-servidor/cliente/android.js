c7e8685bb6860464e98061112a9e36ea24495430
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-09-06 03:07:47 -0300

    Header request com Accept configurado para receber JSON

--- apps-cliente-servidor/cliente/android.js
+++ apps-cliente-servidor/cliente/android.js
@@ -1,19 +1,28 @@
 var http = require("http");
 
+var opcoes = {
+    hostname : "localhost",             // endereço padrão
+    port : 3000,                        // porta do server
+    path : "/",                         // local padrão
+    headers : {
+        "Accept" : "application/json"   // propriedade do header
+    }
+}
+
 var buffer_corpo_response = [];
 
-http.get('http://localhost:3000', function(res){
+http.get(opcoes, function(res){
 
-    res.on('data', function(chunk){     // estado 'data' existe enquanto a requisição está em curso
+    res.on('data', function(chunk){ // estado 'data' existe enquanto a requisição está em curso
         buffer_corpo_response.push(chunk);
     });
 
-    res.on('end', function(){           // 'end' dispara o evento apenas ao término do carregamento da página
+    res.on('end', function(){       // 'end' dispara o evento apenas ao término do carregamento da página
         var corpo_response = Buffer.concat(buffer_corpo_response).toString();
         console.log(corpo_response);
     });
 
-    res.on('error', function(){         // 'error' dispara quando ocorrem erros no fluxo normal
+    res.on('error', function(){     // 'error' dispara quando ocorrem erros no fluxo normal
 
     });
 });
