59d9662fd2c605c8e15580658f7bcffecc14375a
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-09-07 03:39:02 -0300

    Verbo POST para a URI "/api" criado

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -1,6 +1,6 @@
 var express = require("express"),
     bodyParser = require("body-parser"),
-    mongodb = require("mongodb");
+    mongo = require("mongodb");
 
 var app = express();
 
@@ -12,8 +12,32 @@ var port = 8080;
 
 app.listen(port);
 
-console.log("HTTP Server on port"+port);
+var db = new mongo.Db(
+    "instaclone",
+    new mongo.Server("localhost", 27017, {}),
+    {}
+);
+
+console.log("HTTP Server on port: "+port);
 
 app.get("/", function(req, res){
     res.send({ msg : "Olá" });
+});
+
+app.post("/api", function(req, res){
+    var dados = req.body;
+
+    db.open(function(err, mongoclient){
+        mongoclient.collection("posts", function(err, collection){
+            collection.insert(dados, function(err, result){
+                if(err){
+                    res.send(err);
+                }
+                else {
+                    res.send(result);
+                }
+                mongoclient.close();
+            });
+        });
+    });
 });
