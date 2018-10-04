01f20acc2cc053a645f362970b12b650a2a61f74
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-10-04 03:52:26 -0300

    Botões para deletar comentários criados

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -166,15 +166,24 @@ app.put("/api/:id", function (req, res) {
 app.delete("/api/:id", function (req, res) {
     db.open(function (err, mongoclient) {
         mongoclient.collection("posts", function (err, collection) {
-            collection.remove({ _id: objectID(req.params.id) }, function(err, result){
-                if(err){
-                    res.json(err);
+            collection.update(
+                {},
+                { $pull :   {
+                                comentarios : { id_comentario : objectID(req.params.id) }
+                            }
+                },
+                { multi : true },
+            
+                function(err, result){
+                    if(err){
+                        res.json(err);
+                    }
+                    else {
+                        res.json(result);
+                    }
+                    mongoclient.close();
                 }
-                else {
-                    res.json(result);
-                }
-                mongoclient.close();
-            });
+            );
         });
     });
 });
